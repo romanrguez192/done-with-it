06eacdce5050752b2d6137f1f3cde4c9f5e04352
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-10-10 18:33:47 -0400

    Improved InitialScreen

--- src/screens/InitialScreen.jsx
+++ src/screens/InitialScreen.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Image } from "react-native";
 
+import Screen from "../components/Screen";
 import MyText from "../components/MyText";
 import MyButton from "../components/MyButton";
-import colors from "../config/colors";
 
 function InitialScreen() {
   return (
-    <View style={styles.background}>
+    <Screen style={styles.screen}>
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
         <MyText style={styles.title}>Sell What You Don't Need</MyText>
@@ -16,16 +16,14 @@ function InitialScreen() {
         <MyButton>Login</MyButton>
         <MyButton color="secondary">Sign Up</MyButton>
       </View>
-    </View>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: "flex-end",
+  screen: {
+    justifyContent: "space-around",
     alignItems: "center",
-    backgroundColor: colors.light,
   },
   buttonsContainer: {
     padding: 15,
@@ -36,8 +34,7 @@ const styles = StyleSheet.create({
     height: 100,
   },
   logoContainer: {
-    position: "absolute",
-    top: 80,
+    paddingTop: 30,
     alignItems: "center",
   },
   title: {
