22298be56c2a70eb24f9ff7353c8c92d14e5e4d8
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-10-29 13:27:39 -0400

    Status bar

--- App.jsx
+++ App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 import navigationTheme from "./src/navigation/navigationTheme";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { colors } from "./src/config/styles";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+      <StatusBar style="light" backgroundColor={colors.primary} />
+    </>
   );
 }
